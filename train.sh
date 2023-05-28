ed2a71a4c46efdb77fca47ac0d8b97a643404382
Michael Donovan <michael@mdonovan.dev>
2023-05-28 13:13:26 -0500

    Add train.sh

---
+++ train.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+CONFIG=$1
+INPUT=$2
+OUTPUT=$3
+python phasealign.py $INPUT $OUTPUT
+python prep_wav.py "$CONFIG" -n 1 -rs 20 -s "Data/aligned/$(basename $INPUT)" "Data/aligned/$(basename $OUTPUT)" && python dist_model_recnet.py -l "RNN3-$CONFIG" --validation_p 200 --pre_filt None -lm 0
+output_fbname=$(basename "$OUTPUT" | cut -d. -f1)
+mv "Results/$CONFIG/model_best.json" "Results/$CONFIG/$output_fbname_best.json"
